95cf27b510792a7ec665919cbc53a7d133d6c329
babaGie <rylepio79@gmail.com>
2022-05-09 18:45:06 +0800

    import phpmailer

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 </head>
 <body>
     <?php
+        //Import PHPMailer classes into the global namespace
+        //These must be at the top of your script, not inside a function
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+
+        //Load Composer's autoloader
+        require 'vendor/autoload.php';
+
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
         echo 'hello worldssss';
     ?>
 </body>
