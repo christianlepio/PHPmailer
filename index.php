19b6c556b5fd252e6ae2f8d15e978b17aae5e7a1
babaGie <rylepio79@gmail.com>
2022-05-09 19:05:34 +0800

    add attachments

--- index.php
+++ index.php
@@ -37,6 +37,9 @@
             $mail->addCC('cc@example.com');
             $mail->addBCC('bcc@example.com');
         
+            //Attachments
+            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
         }catch(Exception $e){
 
         }
