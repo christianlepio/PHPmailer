607a7110aad4367635bdd85a3cbe7c1fe8065ca0
babaGie <rylepio79@gmail.com>
2022-05-13 21:33:25 +0800

    update first input type

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <form action="" method="post">
             <div style="width: 75vmin; border:1px solid black;">
             <div class="row justify-content-center">
-                <div class="col-md-5">
+                <div class="col-md-11">
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" required>
                         <label for="floatingInput">Your Name<span class="text-danger"> *</span></label>
