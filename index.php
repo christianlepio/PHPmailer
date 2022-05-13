2069a2a6080830a262334640fd921187f1f2571e
babaGie <rylepio79@gmail.com>
2022-05-13 21:36:32 +0800

    update 3rd user input

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-md-5">
+                <div class="col-md-10">
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                         <label for="floatingInput">Subject</label>
