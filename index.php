4dafd6bd01057a46da5a5cda40bdc2ed0b01dfc1
babaGie <rylepio79@gmail.com>
2022-05-13 21:02:09 +0800

    remove colon

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                 <div class="col-md-5">
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Subject:</label>
+                        <label for="floatingInput">Subject</label>
                     </div>
                 </div>
             </div>
