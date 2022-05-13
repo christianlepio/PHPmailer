d85527922fcc8aba8384cd15d09aa1ade81b966e
babaGie <rylepio79@gmail.com>
2022-05-13 21:41:38 +0800

    update column padding

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-md-11">
+                <div class="col-md-11 px-4">
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                         <label for="floatingInput">Subject</label>
