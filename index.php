a4b956cbd4b6126b63c4c6cf96c40185c6fc9664
babaGie <rylepio79@gmail.com>
2022-05-14 00:06:46 +0800

    reemove d-flex class

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
                     </div>
                 </div>
                 <div class="row justify-content-center">
-                    <div class="col-md-11 px-4 d-flex justify-content-center">
+                    <div class="col-md-11">
                         <button class="mt-4 mb-1" type="submit" name="sendmail">SEND</button>
                     </div>
                 </div>
