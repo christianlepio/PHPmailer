b0029ade6c85cebdc00726aeb838dcc9149f7b12
babaGie <rylepio79@gmail.com>
2022-05-13 20:16:36 +0800

    replace position of row anf col

--- index.php
+++ index.php
@@ -13,11 +13,10 @@
     ?>
     <div class="container">
         <form action="" method="post">
-            
+            <div class="row justify-content-center">
+                <div class="col-md-5"></div>
+            </div>
         </form>
-        <div class="row justify-content-center">
-            <div class="col-md-5"></div>
-        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
