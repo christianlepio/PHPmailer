1f7c9df320d67c8ecede88151946e31a9714fc3b
babaGie <rylepio79@gmail.com>
2022-05-13 20:13:59 +0800

    add justify content center

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
         require 'sendmail.php';
     ?>
     <div class="container">
-        
+        <div class="row justify-content-center">
+
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
