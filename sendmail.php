09661a50a8af5b05365ece023f6815f0f946e7a6
babaGie <rylepio79@gmail.com>
2022-05-15 23:55:01 +0800

    add variabels

--- sendmail.php
+++ sendmail.php
@@ -12,7 +12,10 @@
         //Create an instance; passing `true` enables exceptions
         $mail = new PHPMailer(true);
 
-
+        $yname = $_POST[''];
+        $ymail = $_POST[''];
+        $ysubj = $_POST[''];
+        $ymessage = $_POST[''];
 
         try{
             //setup server
