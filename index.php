c1d6a2611176417f6978de2d6b80aa0a311b8010
babaGie <rylepio79@gmail.com>
2022-05-16 00:45:53 +0800

    remove sweet alert cdn

--- index.php
+++ index.php
@@ -59,6 +59,5 @@
         require 'sendmail.php';
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.14/dist/sweetalert2.all.min.js"></script>
 </body>
 </html>
