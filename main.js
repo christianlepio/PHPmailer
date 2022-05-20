d751759e18b57275545c2c7e242f505181b9fbd7
babaGie <rylepio79@gmail.com>
2022-05-20 18:36:23 +0800

    update sweet alert message

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
 
   $( ".btbt" ).on( "click", function(e){
       e.preventDefault();
-      Swal.fire(
-        'Email Sent!',
-        'Your message has been sent successfully!',
-        'success'
-      )
+
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Email Sent!',
+            'Your message has been sent successfully!',
+            'success'
+          )
+        }
+      })
+
+
       window.location.assign("http://localhost/phpmailer/")
   });
