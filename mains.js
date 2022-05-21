a476efebd3477f98cf387494f729a4fc408c0fcd
babaGie <rylepio79@gmail.com>
2022-05-21 16:38:43 +0800

    update timeout value

--- mains.js
+++ mains.js
@@ -20,7 +20,7 @@ $( ".btbt" ).on( "click", function(e){
         })
         setTimeout(function(){
           location.href = "http://localhost/phpmailer/";
-        }, 3500);
+        }, 1500);
         }
       })
   });
